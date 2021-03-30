fe320e923a41818c49f5e11a582b68560440d04e
Jhobany Peña <jhobany.pena@hotmail.com>
2021-03-30 15:21:27 -0500

    Initial commit

---
+++ VirtualEvent.DataBase/Tables/Attachments.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Attachments] (
+    [AttachmentId] INT          IDENTITY (1, 1) NOT NULL,
+    [EventId]      INT          NOT NULL,
+    [Name]         VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Attachments] PRIMARY KEY CLUSTERED ([AttachmentId] ASC),
+    CONSTRAINT [FK_Attachments_Events] FOREIGN KEY ([EventId]) REFERENCES [dbo].[Events] ([EventId])
+);
+
