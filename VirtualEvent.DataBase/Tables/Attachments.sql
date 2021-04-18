387c3aa4a16c0a25205f1134ddfba9f69299bea8
Jhobany Peña <jhobany.pena@hotmail.com>
2021-04-18 11:00:10 -0500

    The database was updated.

--- VirtualEvent.DataBase/Tables/Attachments.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Attachments] (
-    [AttachmentId] INT          IDENTITY (1, 1) NOT NULL,
-    [EventId]      INT          NOT NULL,
-    [Name]         VARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_Attachments] PRIMARY KEY CLUSTERED ([AttachmentId] ASC),
-    CONSTRAINT [FK_Attachments_Events] FOREIGN KEY ([EventId]) REFERENCES [dbo].[Events] ([EventId])
-);
-
---
+++ VirtualEvent.DataBase/Tables/AttendanceAttanchments.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AttendanceAttanchments] (
+    [AttendanceAttachmentId] INT          IDENTITY (1, 1) NOT NULL,
+    [AttendanceId]           INT          NOT NULL,
+    [Name]                   VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_AttendanceAttanchments] PRIMARY KEY CLUSTERED ([AttendanceAttachmentId] ASC),
+    CONSTRAINT [FK_AttendanceAttanchments_Attendance] FOREIGN KEY ([AttendanceId]) REFERENCES [dbo].[Attendance] ([AttendanceId])
+);
+
